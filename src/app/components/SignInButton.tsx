30637bc865cdd7f1f5cd5ec94365e8287a5290b1
Prince Csaba <bolyaicsaba@gmail.com>
2023-10-18 12:02:36 +0200

    just not working

--- src/app/components/SignInButton.tsx
+++ src/app/components/SignInButton.tsx
@@ -1,17 +1,30 @@
 "use client"
 
-import { signIn, signOut, useSession } from 'next-auth/react'
+import { signIn, signOut, useSession, getSession } from 'next-auth/react'
 import React from 'react'
 
 const SignInButton = () => {
 
-  const { data: session } = useSession();
+  const { data: session, status } = useSession()
+
+  console.log('Béla');
+  console.log(session);
+  console.log(status);
+
+/*   if (status === "loading") {
+    return <p>Loading...</p>
+  } */
+
+  if (status === "unauthenticated") {
+    return <p>Access Denied</p>
+  }
 
   if (session && session.user) {
+    console.log(session.user);
     return (
       <div className="flex gap-4 ml-a">
         <p className="text-sky-600">{session.user.name}</p>
-        <button onClick={() => signOut()} className="text-red-600">
+        <button onClick={() => {signOut()}} className="text-red-600">
           Sign Out
         </button>
       </div>
@@ -19,9 +32,17 @@ const SignInButton = () => {
   }
 
   return (
-    <button onClick={() => signIn()} className="text-green-600 ml-auto">
+    <>
+    <button onClick={() => {signOut()}} className="text-red-600">Sign Out</button>
+    <button onClick={() => 
+    {
+      console.log('clicked');
+      signIn()
+    }
+  } className="text-green-600 ml-auto">
     Sign In
   </button>
+      </>
   )
 }
 
